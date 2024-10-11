f0d4d8de2fec4017784714df27ab89e17c5d8793
jchenTPlink <jonathan.chen@tp-link.com>
2024-10-11 09:55:31 -0700

    create network module

---
+++ NetworkService/NetworkService.h
@@ -0,0 +1,18 @@
+//
+//  NetworkService.h
+//  NetworkService
+//
+//  Created by Jonathan Chen on 10/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkService.
+FOUNDATION_EXPORT double NetworkServiceVersionNumber;
+
+//! Project version string for NetworkService.
+FOUNDATION_EXPORT const unsigned char NetworkServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkService/PublicHeader.h>
+
+
